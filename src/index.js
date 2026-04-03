1af4d983c707ba98f532f5a31f4ff3e27dcff4b3
Ansh Majumdar <majumdaransh82@gmail.com>
2026-04-03 12:03:37 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * TermiPet - Main Entry Point
+  * An educational, themeable terminal companion framework
+   */
+
+   const Hatchery = require('./core/hatchery');
+   const { renderPet } = require('./renderer/theme');
+
+   module.exports = {
+     Hatchery,
+       renderPet
+       };
